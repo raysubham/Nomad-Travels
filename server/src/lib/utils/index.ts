b6e8ecc6364b1ad49ed4879879142de8e67211d2
Subham Ray <self.programmed@gmail.com>
2021-08-16 03:15:13 +0530

    authorize function buggggg

--- server/src/lib/utils/index.ts
+++ server/src/lib/utils/index.ts
@@ -1,17 +1,36 @@
+// import { Request } from 'express'
+// import { Database, User } from '../types'
+
+// export const authorize = async (
+//   db: Database,
+//   req: Request
+// ): Promise<User | null> => {
+//   const token: string | undefined = req.get('X-CSRF-TOKEN')
+//     ? req.get('X-CSRF-TOKEN')
+//     : ''
+//   const viewerId: string | null = req.signedCookies.viewer
+//     ? req.signedCookies.viewer
+//     : ''
+
+//   const viewer = await db.users.findOne({
+//     _id: viewerId,
+//     token,
+//   })
+
+//   return viewer
+// }
+
 import { Request } from 'express'
 import { Database, User } from '../types'
 
 export const authorize = async (
   db: Database,
   req: Request
-): Promise<User | null> => {
+): Promise<User | undefined> => {
   const token = req.get('X-CSRF-TOKEN')
   const viewerId = req.signedCookies.viewer
 
-  const viewer = await db.users.findOne({
-    _id: viewerId,
-    token,
-  })
+  const viewer = await db.users.findOne({ _id: { viewerId }, token })
 
   return viewer
 }
